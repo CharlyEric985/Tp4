9101261e16e214d2bcb2b55b3fa5bd83e14da9c8
Eric charly <ericscarfia11@gmail.com>
2024-04-16 23:02:59 +0300

    init

---
+++ src/main/java/mg/flavien/tpbanqueEric/service/GestionnaireCompte.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.flavien.tpbanqueEric.service;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import mg.flavien.tpbanqueEric.entities.CompteBancaire;
+
+/**
+ *
+ * @author flavi
+ */
+@DataSourceDefinition(
+        className = "com.mysql.cj.jdbc.MysqlDataSource",
+        name = "java:app/jdbc/banque",
+        serverName = "localhost",
+        portNumber = 3306,
+        user = "root", // nom et
+        password = "root", // mot de passe que vous avez donnés lors de la création de la base de données
+        databaseName = "banque",
+        properties = {
+            "useSSL=false",
+            "allowPublicKeyRetrieval=true",
+            "driverClass=com.mysql.cj.jdbc.Driver"
+        }
+)
+@ApplicationScoped
+public class GestionnaireCompte {
+
+    @PersistenceContext(unitName = "banquePU")
+    private EntityManager em;
+
+    @Transactional
+    public void creerCompte(CompteBancaire c) {
+        em.persist(c);
+    }
+
+    public List<CompteBancaire> getAllComptes() {
+        Query query = em.createQuery("select c from CompteBancaire as c");
+        return query.getResultList();
+    }
+
+    public int compterCompter() {
+        return this.getAllComptes().size();
+    }
+
+    public CompteBancaire findById(Long idCompte) {
+        return em.find(CompteBancaire.class, idCompte);
+    }
+
+    @Transactional
+    public void transferer(CompteBancaire source, CompteBancaire destination,
+            int montant) {
+        source.retirer(montant);
+        destination.deposer(montant);
+        update(source);
+        update(destination);
+    }
+
+    @Transactional
+    public CompteBancaire update(CompteBancaire compteBancaire) {
+        return em.merge(compteBancaire);
+    }
+
+    @Transactional
+    public void retirerArgent(CompteBancaire source, int montant) {
+        source.retirer(montant);
+        update(source);
+    }
+
+    @Transactional
+    public void deposerArgent(CompteBancaire source, int montant) {
+        source.deposer(montant);
+        update(source);
+    }
+
+    @Transactional
+    public void supprimerCompte(CompteBancaire compte) {
+        em.remove(em.merge(compte));
+    }
+    
+    @Transactional
+   public void modifierNom(CompteBancaire compte,String nom){
+       compte.setNom(nom);
+       update(compte);
+   }
+}
